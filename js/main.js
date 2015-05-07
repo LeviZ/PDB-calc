b28047880e3b78031c68680d05b3632410a3520a
Scott Trenda <Scott.Trenda@sportngin.com>
2015-05-07 00:19:57 -0500

    Refactor and handle prebuilt options

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
-$('#calc').click(function() {
-  function degrees(angle) { return angle * Math.PI / 180 }
+(function() {
+  $('#prebuilt-opt').change(function() {
+    var opts = $(this).val().split(',')
+    calc(+opts[0], +opts[1], opts[2], opts[3], opts[4])
+  })
 
-  var size = $('#size').val()
-  var shape = $('#shape').val()
-  var w = +$('#width').val()
-  var h = +$('#height').val()
-  var b = .5 // bend tolerance
-  var g = size == 's' ? 4 : 7 // wing length
-  var a = shape == 'square' ? 90 : size == 's' ? 83 : 85 // bend angle
+  $('#calc').click(function() {
+    calc(+$('#width').val(), +$('#height').val(), $('#size').val(), $('#shape').val(), '')
+  })
 
-  var x = h / Math.tan(degrees(a)) // half the difference between the base and top
-  var s = h / Math.sin(degrees(a)) // length of one side
+  function calc(w, h, size, shape, idPrefix) {
+    function degrees(angle) { return angle * Math.PI / 180 }
 
-  var L = w - 2*x + 2*s + 2*g + 4*b // total length
-  var b1 = g + b/2
-  var b2 = g + s + 3*b/2
+    var b = .5 // bend tolerance
+    var g = size == 's' ? 4 : 7 // wing length
+    var a = shape == 'square' ? 90 : size == 's' ? 83 : 85 // bend angle
 
-  $('#length').text(L.toFixed(2) + '"')
-  $('#bends').text([b1, b2].map(function(n) { return n.toFixed(2) + '"' }).join(', '))
-})
+    var x = h / Math.tan(degrees(a)) // half the difference between the base and top
+    var s = h / Math.sin(degrees(a)) // length of one side
+
+    var L = w - 2*x + 2*s + 2*g + 4*b // total length
+    var b1 = g + b/2
+    var b2 = g + s + 3*b/2
+
+    $('#' + idPrefix + 'length').text(L.toFixed(2) + '"')
+    $('#' + idPrefix + 'bends').text([b1, b2].map(function(n) { return n.toFixed(2) + '"' }).join(', '))
+  }
+})()
